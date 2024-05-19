03cb566bc7969a9aca051f9a30770af7b27ec79a
Aquarel <54329158+ThatAquarel@users.noreply.github.com>
2024-05-19 10:20:15 -0400

    rewrite timer

--- routine.go
+++ routine.go
@@ -3,6 +3,52 @@ package main
 import "time"
 
 var (
-	running = false
-	timer   = time.NewTimer(0)
+	gpioMap        = map[string]int{"f": 14, "bp": 15, "bs": 18}
+	running        = false
+	timer          = time.NewTimer(0)
+	hardware_error = false
 )
+
+type Zone struct {
+	zone     string
+	duration int
+}
+
+func runRoutine(zones []Zone) {
+	running = true
+	defer stopRoutine()
+
+	for _, zone := range zones {
+		var err error
+		err = allOff()
+		err = setPin(gpioMap[zone.zone], true)
+
+		if err != nil {
+			hardware_error = true
+		}
+
+		timerCurrent := time.NewTimer(time.Duration(zone.duration) * time.Second)
+		timer = timerCurrent
+		<-timerCurrent.C
+
+		if !running {
+			break
+		}
+	}
+}
+
+func stopRoutine() {
+	running = false
+	timer.Stop()
+	allOff()
+}
+
+func allOff() error {
+	var err error
+
+	for zone := range gpioMap {
+		err = setPin(gpioMap[zone], false)
+	}
+
+	return err
+}
